2847a923e63dad322e14ea1cf930e2957ec79158
Paulo Moreira Rabelo <paulomoreirarabelo@Mac-mini.local>
2026-01-23 00:28:20 +0100

    Change weight input from 5kg increments to 1kg increments

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -106,7 +106,7 @@ const ExerciseCard = ({ exercise, onChange, previousWorkout, onSave, scrollToNex
     }, 300);
   };
 
-  const weightOptions = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 280, 300];
+  const weightOptions = Array.from({ length: 301 }, (_, i) => i);
   const repOptions = Array.from({ length: 30 }, (_, i) => i + 1);
 
   return (
