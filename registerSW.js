8544a841d7e70b14090add126bc35243ec39be16
rocopongarecords-maker <rocopongarecords-maker@users.noreply.github.com>
2026-03-20 20:01:16 +0000

    deploy: 3739d8e5fc200912f993781bb1714b5c47b4d04e

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/workout-tracker/sw.js', { scope: '/workout-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
